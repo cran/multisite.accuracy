d20a679cf8dc5e1b6c73121309ca360aaffcbcd9
Joaquim Radua <quimradua@gmail.com>
2024-07-31 18:10:02 +0000

    version 1.3

--- R/source.R
+++ R/source.R
@@ -1,3 +1,110 @@
+.adapted_AROC.sp <-
+function (formula.healthy, group, tag.healthy, data, est.surv.h = c("normal", 
+    "empirical"), p = seq(0, 1, l = 101), B = 1000) 
+{
+    est.surv.h <- match.arg(est.surv.h)
+    np <- length(p)
+    if (inherits(formula.healthy, "character")) {
+        formula.healthy <- as.formula(formula.healthy)
+    }
+    marker <- all.vars(formula.healthy)[1]
+    compute.ROC <- function(formula.healthy, group, tag.healthy, 
+        data, est.surv.h, p = seq(0, 1, l = 101)) {
+        data.h <- data[data[, group] == tag.healthy, ]
+        data.d <- data[data[, group] != tag.healthy, ]
+        n0 <- nrow(data.h)
+        n1 <- nrow(data.d)
+        np <- length(p)
+        marker <- all.vars(formula.healthy)[1]
+        fit0p <- lm(formula = formula.healthy, data = data.h)
+        sigma0p <- summary(fit0p)$sigma
+        pre.placement.values <- (data.d[, marker] - predict(fit0p, 
+            newdata = data.d))/sigma0p
+        if (est.surv.h == "normal") {
+            u1 <- 1 - pnorm(pre.placement.values)
+        }
+        else {
+            res0p <- fit0p$residuals/sigma0p
+            F0res <- ecdf(res0p)
+            u1 <- 1 - F0res(pre.placement.values)
+        }
+        arocp <- numeric(np)
+        for (i in 1:np) {
+            arocp[i] <- sum(u1 <= p[i])/n1
+        }
+        aarocp <- .adapted_simpson(arocp, p)
+        res <- list()
+        res$p <- p
+        res$ROC <- arocp
+        res$AUC <- aarocp
+        res$fit <- fit0p
+        res$data.h <- data.h
+        res$data.d <- data.d
+        res
+    }
+    res.fit <- compute.ROC(formula.healthy = formula.healthy, 
+        group = group, tag.healthy = tag.healthy, data = data, 
+        est.surv.h = est.surv.h, p = p)
+    arocp <- res.fit$ROC
+    aarocp <- res.fit$AUC
+    if (B > 0) {
+        arocpb <- matrix(0, nrow = np, ncol = B)
+        aarocpb <- numeric(B)
+        for (l in 1:B) {
+            data.boot.d <- res.fit$data.d[sample(nrow(res.fit$data.d), 
+                replace = TRUE), ]
+            data.boot.h <- res.fit$data.h
+            res.h.b <- sample(res.fit$fit$residuals, replace = TRUE)
+            data.boot.h[, marker] <- res.fit$fit$fitted + res.h.b
+            data.boot <- rbind(data.boot.d, data.boot.h)
+            res.boot <- compute.ROC(formula.healthy = formula.healthy, 
+                group = group, tag.healthy = tag.healthy, data = data.boot, 
+                est.surv.h = est.surv.h, p = p)
+            arocpb[, l] <- res.boot$ROC
+            aarocpb[l] <- res.boot$AUC
+        }
+    }
+    columns <- switch(as.character(B > 0), `TRUE` = 1:3, `FALSE` = 1)
+    col.names <- c("est", "ql", "qh")[columns]
+    poolROC <- matrix(0, ncol = length(columns), nrow = np, dimnames = list(1:np, 
+        col.names))
+    poolROC[, 1] <- arocp
+    AUC <- aarocp
+    if (B > 0) {
+        poolROC[, 2] <- apply(arocpb, 1, .adapted_ql)
+        poolROC[, 3] <- apply(arocpb, 1, .adapted_qh)
+        AUC <- c(AUC, quantile(aarocpb, c(0.025000000000000001, 
+            0.97499999999999998)))
+    }
+    names(AUC) <- col.names
+    res <- list()
+    res$call <- match.call()
+    res$p <- p
+    res$ROC <- poolROC
+    res$AUC <- AUC
+    res$fit.h <- res.fit$fit
+    res$est.surv.h <- est.surv.h
+    class(res) <- c("AROC", "AROC.sp")
+    res
+}
+.adapted_qh <-
+function (x) 
+{
+    quantile(x, 0.97499999999999998)
+}
+.adapted_ql <-
+function (x) 
+{
+    quantile(x, 0.025000000000000001)
+}
+.adapted_simpson <-
+function (ROC, set.p) 
+{
+    l.set.p <- length(set.p)
+    integral <- (set.p[l.set.p] - set.p[1])/(l.set.p - 1)/3 * 
+        (ROC[1] + ROC[l.set.p] + 4 * sum(ROC[seq(2, l.set.p - 
+            1, by = 2)]) + 2 * sum(ROC[seq(3, l.set.p - 2, by = 2)]))
+}
 multisite.accuracy <-
 function (y, y.pred, site, estimate = c("auc", "bac", "cor", 
     "hr", "mse"), site.method = "covar", mixed = FALSE, min.n = 10, 
@@ -88,11 +195,11 @@ function (y, y.pred, site, estimate = c("auc", "bac", "cor",
     }
     if (estimate == "auc") {
         auc = switch(site.method, covar = {
-            m = AROC.sp(formula.healthy = y.pred ~ site, group = "y", 
-                tag.healthy = 0, data = data.frame(y, y.pred, 
-                  site), ...)
+            m = .adapted_AROC.sp(formula.healthy = y.pred ~ site, 
+                group = "y", tag.healthy = 0, data = data.frame(y, 
+                  y.pred, site), ...)
             auc = unname(m$AUC[1])
-            attr(auc, "method") = "AROC.sp"
+            attr(auc, "method") = "adapted AROC.sp"
             auc
         }, meta = {
             yi = vi = c()
